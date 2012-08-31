74e3508582e1810c03189f03076cc44ca4726eba
Richard van Aalst <richardvanaalst@i-aspect.com>
2012-08-31 16:01:14 +0200

    Setup code for improvement.

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
 	var Slider = [
-		slideWidth = $(".slide").outerWidth(),
 		totalSlides = $(".slide").length,
+		slideWidth = $(".slide").outerWidth(),
 		currentSlide = 0,
-		loop = (window.loop != undefined)? window.loop : true,
 		scrollIncrement = 1,
 		scrollSpeed = 600,
 		showDuration = 1500,
 		sliderRunInterval = false,
 		isPlaying = false,
+		loop = (window.loop != undefined)? window.loop : true,
 		debug = (window.debug != undefined)? window.debug : false
 	];
 		console.log("Slider: "+Slider);
@@ -30,7 +30,16 @@ $(document).ready(function() {
 		// Add play-pause toggle event
 		if ($("#slider-playpause")) {
 			$("#slider-playpause").click(function() {
-				sliderPlayPause();
+				if (isPlaying === true) {
+					sliderPause();
+					$(this).removeClass("play").addClass("pause");
+					isPlaying = false;
+				}
+				else {
+					sliderPlay();
+					$(this).removeClass("pause").addClass("play");
+					isPlaying = true;
+				}
 				return false;
 			});
 		}
@@ -62,7 +71,7 @@ $(document).ready(function() {
 		$("#slider-content").hover(
 			function() {
 				if (debug) console.log("#slider-content mouse over");
-				sliderStop();
+				sliderPause();
 			},
 			function() {
 				if (debug) console.log("#slider-content mouse out");
@@ -74,7 +83,7 @@ $(document).ready(function() {
 		/* TODO: doesnot work this way
 		$(window).blur(function() {
 			if (debug) console.log("Window blur");
-			sliderStop();
+			sliderPause();
 		});
 		$(window).focus(function() {
 			if (debug) console.log("Window focus");
@@ -83,7 +92,8 @@ $(document).ready(function() {
 		*/
 
 		// Activate slider and go to first slide
-		scrollSlider(1, false);
+		// scrollSlider(1, false);
+		$("#slider-playpause").click();
 	})();
 
 
@@ -92,50 +102,27 @@ $(document).ready(function() {
 	};
 
 	function sliderRunReset() {
-		if (typeof sliderRunInterval == "number") {
-			clearInterval(sliderRunInterval);
-				if (debug) console.log("interval cleared: "+sliderRunInterval);
-			sliderRunInterval = false;
-		}
+		clearInterval(sliderRunInterval);
+			if (debug) console.log("interval cleared: "+sliderRunInterval);
+		sliderRunInterval = false;
 	};
 
 
 	function sliderPlay() {
-		sliderRunReset();
+		if (typeof sliderRunInterval == "number") {
+			sliderRunReset();
+		}
 		sliderRunInterval = setInterval(sliderRun, showDuration);
 			if (debug) console.log("interval set: "+sliderRunInterval);
-
-		$("#slider-playpause").removeClass("play").addClass("pause");
-		isPlaying = true;
 	};
 
 	function sliderPause() {
 		sliderRunReset();
-
-		$("#slider-playpause").removeClass("pause").addClass("play");
-		isPlaying = false;
-	};
-
-	function sliderStop() {
-		sliderRunReset();
-
-		$("#slider-playpause").removeClass("pause").addClass("play");
-		isPlaying = false;
-	};
-
-	function sliderPlayPause() {
-		if (isPlaying === true) {
-			sliderPause();
-		}
-		else {
-			sliderPlay();
-		}
 	};
 
 
 	function scrollSlider(slide, click) {
 
-		// sliderRunReset();
 		var newSlide = slide,
 			posNew;
 
@@ -143,50 +130,54 @@ $(document).ready(function() {
 			switch (newSlide) {
 
 				case "prev":
-					switch (loop) {
-						// Add last slide before first
-						case true:
-							//	if (debug) if (currentSlide == 1) console.log("Add last slide before first");
-							//break;
-
-						// Loop back to last slide
-						case "rewind":
-							newSlide = (currentSlide - scrollIncrement < 1)? totalSlides : currentSlide - scrollIncrement;
-								if (debug) if (currentSlide == 1) console.log("Loop back to last slide");
-							break;
-
+					if (loop === false) {
 						// Stop at first slide
-						case false:
-							newSlide = Math.max(1, currentSlide - scrollIncrement);
-								if (debug) if (currentSlide == 1) console.log("Stop at first slide");
-							break;
+						newSlide = Math.max(1, currentSlide - scrollIncrement);
+							if (debug) if (currentSlide == 1) console.log("Stop at first slide");
+					}
+					else {
+						/*if (loop == "rewind" ) {
+							// Loop back to last slide
+							newSlide = '';
+								if (debug) if (currentSlide == 1) console.log("Loop back to last slide");
+						}
+						else {
+							// Add last slide before first
+							newSlide = (currentSlide - scrollIncrement < 1)? totalSlides : currentSlide - scrollIncrement;
+								if (debug) if (currentSlide == 1) console.log("Add last slide before first");
+						}*/
+						/* TEMP */
+						newSlide = (currentSlide - scrollIncrement < 1)? totalSlides : currentSlide - scrollIncrement;
+							if (debug) if (currentSlide == 1) console.log("Loop back to last slide");
 					}
 					break;
 
 				case "next":
-					switch (loop) {
-						// Add first slide after last
-						case true:
-							//	if (debug) if (currentSlide == totalSlides) console.log("Add first slide after last");
-							//break;
-
+					if (loop === false) {
+						// Stop at last slide
+							if (debug) console.log("Will stop at last slide");
+						newSlide = Math.min(totalSlides, currentSlide + scrollIncrement);
+							if (debug) if (currentSlide == totalSlides) console.log("Stop at last slide");
+					}
+					else {
+						/*if (loop == "rewind" ) {
+							// Add first slide after last
+							newSlide = '';
+								if (debug) if (currentSlide == totalSlides) console.log("Add first slide after last");
+						}
+						else {
 						// Loop back to first slide
-						case "rewind":
 							newSlide = (currentSlide + scrollIncrement > totalSlides)? 1 : currentSlide + scrollIncrement;
 								if (debug) if (currentSlide == totalSlides) console.log("Loop back to first slide");
-							break;
-
-						// Stop at last slide
-						case false:
-								if (debug) console.log("Will stop at last slide");
-							newSlide = Math.min(totalSlides, currentSlide + scrollIncrement);
-								if (debug) if (currentSlide == totalSlides) console.log("Stop at last slide");
-							break;
+						}*/
+						/* TEMP */
+						newSlide = (currentSlide + scrollIncrement > totalSlides)? 1 : currentSlide + scrollIncrement;
+							if (debug) if (currentSlide == totalSlides) console.log("Loop back to first slide");
 					}
 					break;
 
 				default:
-					// Pop ID of slide: "#slider-nav-item-1"
+					// Get ID nr of slide: "#slider-nav-item-1"
 					newSlide = newSlide.split("-").pop();
 					break;
 			}
@@ -195,6 +186,7 @@ $(document).ready(function() {
 		// Set new (left) position
 		posNew = -(newSlide-1) * slideWidth;
 			if (debug) console.log("currentSlide: "+currentSlide+ ", newSlide: "+newSlide+ ", posNew: "+posNew);
+
 
 		// Bounce effect at first/last slide when not looping
 		if (loop === false && click === true && (
@@ -225,7 +217,7 @@ $(document).ready(function() {
 			$("#slides").stop(true).animate({"left": posNew}, scrollSpeed, function() {
 				// Clear interval when not looping and on last slide
 				/*if (loop === false && currentSlide == totalSlides) {
-					sliderStop();
+					sliderPause();
 				}
 				else {
 					sliderPlay();
@@ -233,9 +225,10 @@ $(document).ready(function() {
 			});
 		}
 
+
 		// Clear interval when not looping and on last slide
 		if (loop === false && currentSlide == totalSlides) {
-			sliderStop();
+			sliderPause();
 		}
 		else {
 			sliderPlay();
